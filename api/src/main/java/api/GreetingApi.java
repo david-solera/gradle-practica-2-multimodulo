4e64dad660b9c2723dcb81f2e1873ba865a59d42
david-solera <80710002+david-solera@users.noreply.github.com>
2025-12-15 13:29:11 +0000

    Practica 2 terminada

---
+++ api/src/main/java/api/GreetingApi.java
@@ -0,0 +1,10 @@
+package api;
+
+import core.GreetingService;
+
+public class GreetingApi {
+private final GreetingService service = new GreetingService();
+public String buildMessage(String name) {
+return "[API] " + service.greet(name);
+}
+}
